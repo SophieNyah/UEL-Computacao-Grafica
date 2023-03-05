7d21646980c8990c01ae930fbf01baceaaa47b07
Sophie <sophie.nyascimento@gmail.com>
2023-03-05 17:54:26 -0300

    Programa Concluído

--- transformadas/Matrizes.cpp
+++ transformadas/Matrizes.cpp
@@ -2,6 +2,7 @@
 // Created by sophie on 27/02/23.
 //
 
+#include <cmath>
 #include "Matrizes.h"
 
 namespace Matriz {
@@ -12,4 +13,35 @@ namespace Matriz {
         res[2] = mat[2][0]*ponto[0] + mat[2][1]*ponto[1] + mat[2][2]*ponto[2];
         return res;
     }
+
+    double grauPraRadiano(const double grau){
+        return (grau / 180.0) * ((double) PI);
+    }
+
+    double senG(const double grau) {
+        if (!std::isfinite(grau)) {
+            return sin(grau);
+        }
+        if (grau < 0.0) {
+            return -senG(-grau);
+        }
+        int quo;
+        double x90 = remquo(fabs(grau), 90.0, &quo);
+        switch (quo % 4) {
+            case 0:
+                // Use * 1.0 to avoid -0.0
+                return sin(grauPraRadiano(x90)* 1.0);
+            case 1:
+                return cos(grauPraRadiano(x90));
+            case 2:
+                return sin(grauPraRadiano(-x90) * 1.0);
+            case 3:
+                return -cos(grauPraRadiano(x90));
+        }
+        return 0.0;
+    }
+    double cosG(const double grau) {
+        return senG(90 - fabs(grau));
+    }
+
 }
--- transformadas/Matrizes.h
+++ transformadas/Matrizes.h
@@ -11,6 +11,11 @@ namespace Matriz {
     using Mat3_1 = std::array<double, 3>;
     using Mat3_3 = std::array<std::array<double, 3>, 3>;
     Mat3_1 multiplica(const Mat3_3,  const Mat3_1);
+
+    constexpr double PI = 3.1415926535897932384626433832795028841971693993751058209;
+    double senG(const double grau);
+    double cosG(const double grau);
+    double grauPraRadiano(const double grau);
 }
 
 #endif // TRANSFORMADAS_MATRIZES_H
